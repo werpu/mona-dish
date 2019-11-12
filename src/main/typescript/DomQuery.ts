266185cac1a78c62cef3e6f69cf1ad01616178e7
werpu <werner.punz@gmail.com>
2019-11-12 23:12:57 +0100

    smaller fixes from integration tests

--- src/main/typescript/DomQuery.ts
+++ src/main/typescript/DomQuery.ts
@@ -728,10 +728,28 @@ export class DomQuery implements IDomQuery, IStreamDataSource<DomQuery> {
             doc.documentElement.innerHTML = markup;
             return new DomQuery(doc.documentElement);
         } else {
-            doc.body.innerHTML = markup;
-            return new DomQuery(...<Array<Element>>objToArray(doc.body.childNodes));
+
+            let dummyPlaceHolder = new DomQuery(document.createElement("div"));
+
+            //table needs special treatment due to the browsers auto creation
+            if(markup.indexOf("<thead ") == 0 || markup.indexOf("<tbody ") || markup.indexOf("<tfoot ")) {
+                dummyPlaceHolder.html(`<table>${markup}</table>` );
+                return dummyPlaceHolder.querySelectorAll("table").get(0).childNodes.detach();
+            }
+
+            if(markup.indexOf("<tr ") == 0) {
+                dummyPlaceHolder.html(`<table><tbody>${markup}</tbody></table>`);
+                return dummyPlaceHolder.querySelectorAll("tbody").get(0).childNodes.detach();
+            }
+
+            if(markup.indexOf("<td ") == 0) {
+                dummyPlaceHolder.html(`<table><tbody><tr>${markup}</tr></tbody></table>`);
+                return dummyPlaceHolder.querySelectorAll("tr").get(0).childNodes.detach();
+            }
+
+            dummyPlaceHolder.html(markup);
+            return  dummyPlaceHolder.childNodes.detach();
         }
-        // }
     }
 
     /**
@@ -1209,7 +1227,7 @@ export class DomQuery implements IDomQuery, IStreamDataSource<DomQuery> {
 
         let res = [];
         res.push(this);
-        res.concat(toInsertParams);
+        res = res.concat(toInsertParams);
         return new DomQuery(...res);
     }
 
@@ -1225,7 +1243,7 @@ export class DomQuery implements IDomQuery, IStreamDataSource<DomQuery> {
         });
         let res = [];
         res.push(this);
-        res.concat(toInsertParams);
+        res = res.concat(toInsertParams);
         return new DomQuery(...res);
     }
 
@@ -1412,11 +1430,15 @@ export class DomQuery implements IDomQuery, IStreamDataSource<DomQuery> {
                 }
             };
         try {
-            let scriptElements = this.querySelectorAll("script");
-            if (scriptElements == null) return;
-            for (let cnt = 0; cnt < scriptElements.length; cnt++) {
-                execScrpt(scriptElements.getAsElem(cnt).value);
-            }
+            let scriptElements = new DomQuery(this.filterSelector("script"), this.querySelectorAll("script"));
+            //script execution order by relative pos in their dom tree
+            scriptElements.stream
+                .flatMap(item => { return Stream.of(item.values) })
+                .sort((node1, node2) => {
+                    return node1.compareDocumentPosition(node2) - 3; //preceding 2, following == 4
+                })
+                .each(item => execScrpt(item));
+
             if (finalScripts.length) {
                 this.globalEval(finalScripts.join("\n"));
             }
@@ -1481,12 +1503,14 @@ export class DomQuery implements IDomQuery, IStreamDataSource<DomQuery> {
                 }
             };
 
-        const scriptElements: DomQuery = this.querySelectorAll("link, style");
-        if (scriptElements == null) return;
-        for (let cnt = 0; cnt < scriptElements.length; cnt++) {
-            let element: any = scriptElements.getAsElem(cnt);
-            execCss(element.value);
-        }
+        const scriptElements: DomQuery =  new DomQuery(this.filterSelector("link, style"), this.querySelectorAll("link, style"));
+
+        scriptElements.stream
+            .flatMap(item => { return Stream.of(item.values) })
+            .sort((node1, node2) => {
+                return node1.compareDocumentPosition(node2) - 3; //preceding 2, following == 4
+            })
+            .each(item => execCss(item));
 
         return this;
     }
