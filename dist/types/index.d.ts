0e00865a227b0474c71d6022898b72b0cbbca551
werpu <werner.punz@gmail.com>
2020-04-25 22:59:41 +0200

    fixing a this issue

---
+++ dist/types/index.d.ts
@@ -0,0 +1,8 @@
+export { DomQuery, ElementAttribute, DomQueryCollector, DQ } from "./DomQuery";
+export { Lang } from "./Lang";
+export { Config, Monad, IValueHolder, IFunctor, IMonad, IIdentity, Optional, ValueEmbedder } from "./Monad";
+export { CancellablePromise, Promise, IPromise, PromiseStatus } from "./Promise";
+export { XMLQuery, XQ } from "./XmlQuery";
+export { Stream, LazyStream, IteratableConsumer, IStream } from "./Stream";
+export { ArrayStreamDataSource, MappedStreamDataSource, FilteredStreamDatasource, FlatMapStreamDataSource, QueryFormStringCollector, IStreamDataSource, ICollector, ArrayCollector, AssocArrayCollector, FormDataCollector, QueryFormDataCollector } from "./SourcesCollectors";
+export { TagBuilder } from "./TagBuilder";
